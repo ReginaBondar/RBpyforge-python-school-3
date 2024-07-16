464cb3b4e7d68a2d78bb52cd6f5eb2e5ba4e625c
ReginaBondar <Fanisowna.regina@yandex.ru>
2024-07-16 22:24:07 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,2 @@
-from rdkit import Chem
-
 def substructure_search(mols, mol):
-    molecule = Chem.MolFromSmiles(mol)
-    molecules_container = [Chem.MolFromSmiles(smiles) for smiles in mols]
-    substructure_result = [molec for molec in molecules_container if molec.HasSubstructMatch(molecule)]
-    return substructure_result
+    pass
