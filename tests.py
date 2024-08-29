876895b512084ada019b3669876cea01d2db3a71
ReginaBondar <Fanisowna.regina@yandex.ru>
2024-08-30 01:33:14 +0300

    tests.py

--- tests.py
+++ tests.py
@@ -15,35 +15,33 @@ def test_add_molecule(molecule_id, mol, expected):
         add_molecule(1, "CCO")
 
 
-@pytest.mark.parametrize("molecule_id, mol, expected", [(5, "CC(=O)", [{"molecule_id": 3, "molecule": "CC(=O)O"},
+@pytest.mark.parametrize("molecule_id, mol, limit, expected", [(5, "CC(=O)", 2, [{"molecule_id": 3, "molecule": "CC(=O)O"},
                                                                        {"molecule_id": 4,
                                                                         "molecule": "CC(=O)Oc1ccccc1C(=O)O"}]),
-                                                        (6, "c1ccccc1", [{"molecule_id": 2, "molecule": "Cc1ccccc1"},
-                                                                         {"molecule_id": 4,
-                                                                          "molecule": "CC(=O)Oc1ccccc1C(=O)O"}])])
-def test_substructure_search(molecule_id, mol, expected):
+                                                        (6, "c1ccccc1", 1, [{"molecule_id": 2, "molecule": "Cc1ccccc1"}])])
+def test_substructure_search(molecule_id, mol, limit, expected):
     try:
         add_molecule(molecule_id, mol)
     except HTTPException:
         print("The molecule is already in the molecule database")
     finally:
-        result = substructure_search(molecule_id, mol)
+        result = substructure_search(molecule_id, mol, limit)
         assert result == expected
 
     with pytest.raises(HTTPException):
-        substructure_search(10, "O")
+        substructure_search(10, "O", 2)
 
 
 def test_substructure_search2():
     try:
-        substructure_search(10, 'CC(=O)')
+        substructure_search(10, 'CC(=O)', 1)
     except HTTPException:
         print("Molecule is not added")
 
 
 @pytest.mark.xfail
 def test_substructure_search3():
-    assert substructure_search(5, 'CC(=O)') == [{"molecule_id": 3, "molecule": "CC(=O)O"}]
+    assert substructure_search(5, 'CC(=O)', 1) == [{"molecule_id": 3, "molecule": "CC(=O)O"}]
 
 
 @pytest.mark.parametrize("molecule_id, expected", [(5, {"molecule_id": 5, "molecule": "CC(=O)"}),
