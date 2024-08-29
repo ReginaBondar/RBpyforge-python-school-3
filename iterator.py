754c949f9243d63444a5daf1a0242d7fafc793f2
ReginaBondar <Fanisowna.regina@yandex.ru>
2024-08-30 01:18:33 +0300

    iterator.py

--- iterator.py
+++ iterator.py
@@ -1,16 +1,26 @@
+from rdkit import Chem
+
+
 class IteratorMolecules:
-    def __init__(self, sequence, stop=1):
+    def __init__(self, sequence, molecule, stop=1):
         self._sequence = sequence
         self._stop = stop
-        self._index = 0
+        self._mol = molecule
+        self._index = 0  # счетчик для перебора элементов последовательности
+        self._counter = 0  # количество возвращенных значений
+
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self._index < self._stop:
-            molecule = self._sequence[self._index]
-            self._index += 1
-            return molecule
+        if self._counter < self._stop and self._index < len(self._sequence):
+            if self._sequence[self._index].HasSubstructMatch(self._mol):
+                item = self._index
+                self._index += 1
+                self._counter += 1
+                return item
+            else:
+                self._index += 1
         else:
             raise StopIteration
